9c8704957c5dcde301e1c144ead94e98b1659d01
Cris <crisnavas10@gmail.com>
2020-05-20 23:18:58 +0200

    Descomentada línea

--- Facturas/src/Clases_Factura/Factura.java
+++ Facturas/src/Clases_Factura/Factura.java
@@ -3,7 +3,7 @@ private class Factura {
 	public int num;
 	public float base;
 	public flota tipoIva;
-	//public Estados estado;
+	public Estados estado;
 	
 	public void static main(String[] args) {
 
